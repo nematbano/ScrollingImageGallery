a37b93f09d3ee920ae40d303a4257de01b442594
Nemat Bano <nematbano@Nemats-MBP.lan>
2024-07-19 15:51:04 +0800

    Add a toolbar on Main Activity

--- app/src/main/java/com/example/scrollingimagegallery/MainActivity.kt
+++ app/src/main/java/com/example/scrollingimagegallery/MainActivity.kt
@@ -1,19 +1,26 @@
 package com.example.scrollingimagegallery
 
 import android.os.Bundle
+
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.example.scrollingimagegallery.recyclerview.ImageGalleryAdapter
 
 class MainActivity : AppCompatActivity() {
     private lateinit var swipeRefreshLayout: SwipeRefreshLayout
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: ImageGalleryAdapter
+    private lateinit var toolbar: Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        swipeRefreshLayout = findViewById(R.id.container)
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = getString(R.string.toolbar_title)
+        swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout)
         recyclerView = findViewById(R.id.recyclerView)
 
         adapter = ImageGalleryAdapter()
